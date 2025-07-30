52d9a49215289996a523c9f99a74de35e2f5a810
Samual Norman <me@samual.uk>
2025-07-30 09:26:02 +0100

    remove `exports.js` and hardcode `exports` field for now also make sure to emit `name` in `jsr.json` as JSR-compatible name

--- scripts/emit-jsr-json.js
+++ scripts/emit-jsr-json.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 import { mkdirSync as makeDirectorySync, writeFileSync } from "fs"
 import packageJson from "../package.json" with { type: "json" }
-import { getExports } from "./lib/exports.js"
 
 /** @type {Record<string, string>} */ const ConvertToJsr = {
 	"@samual/types": "@samual/types"
 }
 
-const { name, version, license, dependencies } = packageJson
+const { version, license, dependencies } = packageJson
 
 makeDirectorySync("dist", { recursive: true })
 
@@ -17,7 +16,7 @@ const imports = Object.fromEntries(Object.entries(dependencies).map(
 
 writeFileSync(
 	"dist/jsr.json",
-	JSON.stringify({ name, version, license, exports: await getExports(`.d.ts`, `.js`), imports }, undefined, "\t")
+	JSON.stringify({ name: `@sn/tulid`, version, license, exports: { ".": "./default.ts" }, imports }, undefined, "\t")
 )
 
 process.exit()
