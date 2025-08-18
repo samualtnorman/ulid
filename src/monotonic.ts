a2d67395ac6be6e69e5088f009a57cccf7f2163e
Samual Norman <me@samual.uk>
2025-08-18 17:01:34 +0100

    fix failing jsr publish by removing `.ts` from import

--- src/monotonic.ts
+++ src/monotonic.ts
@@ -1,4 +1,4 @@
-import { makeMonotonicallyIncrementingUlidBufferFunction, makeUlid as makeUlid_, type Ulid, type UlidBuffer } from "./default.ts"
+import { makeMonotonicallyIncrementingUlidBufferFunction, makeUlid as makeUlid_, type Ulid, type UlidBuffer } from "./default"
 
 export const makeUlidBuffer: () => UlidBuffer = makeMonotonicallyIncrementingUlidBufferFunction()
 export const makeUlid = (): Ulid => makeUlid_({ ulidBuffer: makeUlidBuffer() })
