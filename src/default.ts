c02ebacf682f896354faeb03d65408b3127d99af
Samual Norman <me@samual.uk>
2025-08-19 10:29:23 +0100

    docs

--- src/default.ts
+++ src/default.ts
@@ -3,17 +3,17 @@ import type { Brand, LaxPartial } from "@samual/types"
 const CROCKFORD_BASE32 = `0123456789ABCDEFGHJKMNPQRSTVWXYZ`
 const ULID_REGEX = /^[0-7][\dA-HJKMNP-TV-Z]{25}$/
 
-/** A 16-byte `ArrayBuffer`. */
+/** A 16-byte {@linkcode ArrayBuffer}. */
 export type UlidBuffer = Brand<ArrayBuffer, { readonly UlidBuffer: unique symbol }[`UlidBuffer`]>
 
 /** A valid [ULID](https://github.com/ulid/spec#readme) string. */
 export type Ulid = Brand<string, { readonly Ulid: unique symbol }[`Ulid`]>
 
-/** Check if the `ArrayBuffer`'s length is 16. */
+/** Check if the given {@linkcode ArrayBuffer}'s length is 16, if so narrow it to a {@linkcode UlidBuffer}. */
 export const isUlidBuffer =
 	(arrayBuffer: ArrayBuffer): arrayBuffer is UlidBuffer => arrayBuffer.byteLength == 16
 
-/** If the `ArrayBuffer` has a length of 16, it'll be returned as a `UlidBuffer`, otherwise throws. */
+/** If the {@linkcode ArrayBuffer} has a length of 16, it'll be returned as a {@linkcode UlidBuffer}, otherwise throws. */
 export const toUlidBuffer = (arrayBuffer: ArrayBuffer): UlidBuffer => {
 	if (isUlidBuffer(arrayBuffer))
 		return arrayBuffer
@@ -21,13 +21,10 @@ export const toUlidBuffer = (arrayBuffer: ArrayBuffer): UlidBuffer => {
 	throw Error(`ArrayBuffer's length was not 16`)
 }
 
-/** Check if the string is a `Ulid`. */
+/** Check if the given string is a valid [ULID](https://github.com/ulid/spec#readme), if so narrow it to a {@linkcode Ulid}. */
 export const isUlid = (string: string): string is Ulid => ULID_REGEX.test(string)
 
-/**
- * If the string is a valid [ULID](https://github.com/ulid/spec#readme), it'll be returned as a `Ulid`, otherwise
- * throws.
- */
+/** If the string is a valid [ULID](https://github.com/ulid/spec#readme), it'll be returned as a `Ulid`, otherwise throws. */
 export const toUlid = (string: string): Ulid => {
 	if (isUlid(string))
 		return string
@@ -36,10 +33,10 @@ export const toUlid = (string: string): Ulid => {
 }
 
 /**
- * Make an empty 16-byte `ArrayBuffer`.
+ * Make an empty 16-byte {@linkcode ArrayBuffer} that's narrowed to a {@linkcode UlidBuffer}.
  *
- * This can be useful if you don't want to use `makeUlid()` or `makeUlidBuffer()` to for example use a custom random
- * function.
+ * This can be useful if you don't want to use {@linkcode makeUlid()} or {@linkcode makeUlidBuffer()} to for example use
+ * a custom random function.
  * @example
  * ```js
  * const ulidBuffer = makeEmptyUlidBuffer()
@@ -55,7 +52,7 @@ export const toUlid = (string: string): Ulid => {
 export const makeEmptyUlidBuffer = (): UlidBuffer => new ArrayBuffer(16) as UlidBuffer
 
 /**
- * Set or update the time of a `UlidBuffer`.
+ * Set or update the time of a {@linkcode UlidBuffer}.
  * @example
  * ```js
  * // Set to a specific time
@@ -73,7 +70,7 @@ export const setUlidBufferTime = (buffer: UlidBuffer, time: number = Date.now())
 }
 
 /**
- * Retrieve the time from a `UlidBuffer`.
+ * Retrieve the time from a {@linkcode UlidBuffer}.
  * @example
  * ```js
  * const ulid = toUlid(`01ARZ3NDEKTSV4RRFFQ69G5FAV`)
@@ -90,10 +87,8 @@ export const getUlidBufferTime = (buffer: UlidBuffer): number => {
 	return (dataView.getUint16(0) * (2 ** (8 * 4))) + dataView.getUint32(2)
 }
 
-export type MakeUlidBufferOptions = { ulidBuffer: UlidBuffer, time: number }
-
 /**
- * Make a `UlidBuffer`.
+ * Make a {@linkcode UlidBuffer}.
  *
  * This can be useful if you need it as an `ArrayBuffer` first, you can then stringify after.
  * @example
@@ -105,19 +100,30 @@ export type MakeUlidBufferOptions = { ulidBuffer: UlidBuffer, time: number }
  * console.log(`Created user with id ${makeUlid({ buffer: ulidBuffer })}`)
  * ```
  */
-export const makeUlidBuffer = (
-	{ ulidBuffer = makeEmptyUlidBuffer(), time = Date.now() }: LaxPartial<MakeUlidBufferOptions> = {}
-): UlidBuffer => {
+export const makeUlidBuffer = ({ ulidBuffer = makeEmptyUlidBuffer(), time = Date.now() }: LaxPartial<{
+	/** Reuse a previously used {@linkcode UlidBuffer}. Will be completely overwritten. @default makeEmptyUlidBuffer() */
+	ulidBuffer: UlidBuffer
+
+	/** The time in milliseconds that'll be written into the buffer. @default Date.now() */ time: number
+}> = {}): UlidBuffer => {
 	setUlidBufferTime(ulidBuffer, time)
 	crypto.getRandomValues(new Uint8Array(ulidBuffer, 6))
 
 	return ulidBuffer
 }
 
-/** Make a [ULID](https://github.com/ulid/spec#readme) string. */
-export const makeUlid = (
-	{ time = Date.now(), ulidBuffer = makeUlidBuffer({ time }) }: LaxPartial<MakeUlidBufferOptions> = {}
-): Ulid => {
+/** Make a [ULID](https://github.com/ulid/spec#readme) string that's narrowed to a {@linkcode Ulid}. */
+export const makeUlid = ({ time = Date.now(), ulidBuffer = makeUlidBuffer({ time }) }: LaxPartial<{
+	/** The {@linkcode UlidBuffer} to turn into a {@linkcode Ulid}. @default makeUlidBuffer() */ ulidBuffer: UlidBuffer
+
+	/**
+	 * The time in milliseconds used when creating the default {@linkcode UlidBuffer} if you didn't pass in your own.
+	 *
+	 * This will have no affect if you passed in your own {@linkcode UlidBuffer}.
+	 * @default Date.now()
+	 */
+	time: number
+}> = {}): Ulid => {
 	const dataView = new DataView(ulidBuffer)
 	let result = CROCKFORD_BASE32[dataView.getUint8(0) >> 5]!
 
@@ -130,7 +136,7 @@ export const makeUlid = (
 	return (result + CROCKFORD_BASE32[dataView.getUint8(15) & 0b1_1111]) as Ulid
 }
 
-/** Turn a ULID string back into an `ArrayBuffer`. */
+/** Turn a {@linkcode Ulid} back into an {@linkcode UlidBuffer}. */
 export const decodeUlid = (ulid: Ulid): UlidBuffer => {
 	const ulidBuffer = makeUlidBuffer()
 	const bytes = new Uint8Array(ulidBuffer)
@@ -157,6 +163,7 @@ export const decodeUlid = (ulid: Ulid): UlidBuffer => {
 	return ulidBuffer
 }
 
+/** Increment the random part of the given {@linkcode UlidBuffer} by 1. */
 export const incrementUlidBuffer = (ulidBuffer: UlidBuffer, { throwOnOverflow = false }: LaxPartial<{ throwOnOverflow: boolean }> = {}): void => {
 	const dataView = new DataView(ulidBuffer)
 
@@ -170,11 +177,25 @@ export const incrementUlidBuffer = (ulidBuffer: UlidBuffer, { throwOnOverflow = 
 	}
 }
 
+/** @returns A clone of the given {@linkcode UlidBuffer}. */
 export const cloneUlidBuffer = (ulidBuffer: UlidBuffer): UlidBuffer => ulidBuffer.slice() as UlidBuffer
 
-export const makeMonotonicallyIncrementingUlidBufferFunction = (
-	{ mitigateOverflow = true }: LaxPartial<{ mitigateOverflow: boolean }> = {}
-): () => UlidBuffer => {
+/**
+ * @returns A function that generates monotonically incrementing {@linkcode UlidBuffer}s. This means if you generate 2
+ * `UlidBuffer`s using the returned function within the same millisecond, the next one'll simply be an incremented
+ * version of the previous one.
+ * @throws If you try to generate another `UlidBuffer` and the previous's random section bytes is all `0xFF`.
+ * This is mitigated with `mitigateOverflow` which is on by default.
+ */
+export const makeMonotonicallyIncrementingUlidBufferFunction = ({ mitigateOverflow = true }: LaxPartial<{
+	/**
+	 * Mitigate the chance of overflow by setting the most significant bit to `0` after generating a new {UlidBuffer}
+	 * (not when incrementing the previously generated `UlidBuffer`). This should in theory guarantee that you have at
+	 * least 604 sextillion increments before you hit the overflow which I imagine is incredibly hard, especially in
+	 * JavaScript.
+	 */
+	mitigateOverflow: boolean
+}> = {}): () => UlidBuffer => {
 	let ulidBuffer: UlidBuffer
 
 	return () => {
