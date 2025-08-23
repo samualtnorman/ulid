c97cdff2fec1cd72abd89d9df419307b90ae9b08
Samual Norman <me@samual.uk>
2025-08-23 01:16:32 +0100

    add profiling monotonic

--- profile.ts
+++ profile.ts
@@ -1,13 +1,15 @@
 import { makeUlid } from "./src/default.ts"
+import * as Monotonic from "./src/monotonic.ts"
 
 declare const button: HTMLButtonElement
+declare const monotonicButton: HTMLButtonElement
 
-function start() {
+function start(makeUlid: () => string) {
 	let result
 
 	console.time()
 
-	for (let count = 10_000_000; count--;)
+	for (let count = 100_000_000; count--;)
 		result = makeUlid()
 
 	console.timeEnd()
@@ -15,7 +17,8 @@ function start() {
 	console.log(result)
 }
 
-if (typeof button == `object`)
-	button.onclick = start
-else
-	start()
+if (typeof document == `object`) {
+	button.onclick = () => start(makeUlid)
+	monotonicButton.onclick = () => start(Monotonic.makeUlid)
+} else
+	start(process.env.MONOTONIC ? Monotonic.makeUlid : makeUlid)
