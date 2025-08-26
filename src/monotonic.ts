b82989a33c1a59ec3cb67a7bf97a2aab08753926
Samual Norman <me@samual.uk>
2025-08-26 15:34:34 +0100

    rewrite monotonic ULID function from scratch its design is inspired by @tauksun's `ulid-generator` it works by avoiding `UlidBuffer`s and instead working on a `Uint8Array` that is directly `TextEncoder#decode()`d into the ULID string

--- src/monotonic.ts
+++ src/monotonic.ts
@@ -1,4 +1,4 @@
-import { makeEmptyUlidBuffer, makeMonotonicallyIncrementingUlidBufferFunction, makeUlid as makeUlid_, type Ulid, type UlidBuffer } from "./default.ts"
+import { makeMonotonicallyIncrementingUlidBufferFunction, type Ulid, type UlidBuffer } from "./default.ts"
 
 /**
  * A shared function used for generating monotonically incrementing {@linkcode UlidBuffer}s.
@@ -8,7 +8,14 @@ import { makeEmptyUlidBuffer, makeMonotonicallyIncrementingUlidBufferFunction, m
  */
 export const makeUlidBuffer = makeMonotonicallyIncrementingUlidBufferFunction()
 
-const persistedUlidBuffer = makeEmptyUlidBuffer()
+let stateTime = 0
+const stateUlidStringBytes = new Uint8Array(26)
+const textDecoder = new TextDecoder()
+
+const CROCKFORD_BASE32_CHAR_CODES = new Uint8Array([
+	48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70,
+	71, 72, 74, 75, 77, 78, 80, 81, 82, 83, 84, 86, 87, 88, 89, 90
+])
 
 /**
  * A shared function used for generating monotonically incrementing {@linkcode Ulid}s.
@@ -16,7 +23,36 @@ const persistedUlidBuffer = makeEmptyUlidBuffer()
  * `Ulid` may appear to "skip" if they call this function in between your code calling this function. If that's
  * fine with you, feel free to use this function out of convenience.
  */
-export const makeUlid = (): Ulid => makeUlid_({ ulidBuffer: makeUlidBuffer({ ulidBuffer: persistedUlidBuffer }) })
+export const makeUlid = (): Ulid => {
+	const time = Date.now()
+
+	if_: if (stateTime < time) {
+		stateTime = time
+
+		for (let offset = 50, index = 0; offset;)
+			stateUlidStringBytes[index++] = CROCKFORD_BASE32_CHAR_CODES[(time / (2 ** (offset -= 5))) & 0x1F]!
+
+		crypto.getRandomValues(new Uint8Array(stateUlidStringBytes.buffer, 10))
+
+		stateUlidStringBytes[10] = CROCKFORD_BASE32_CHAR_CODES[stateUlidStringBytes[10]! & 0xF]!
+
+		for (let index = 11; index < 26;)
+			stateUlidStringBytes[index] = CROCKFORD_BASE32_CHAR_CODES[stateUlidStringBytes[index++]! & 0x1F]!
+	} else {
+		for (let index = 25; index > 9; index--) {
+			const value = (CROCKFORD_BASE32_CHAR_CODES.indexOf(stateUlidStringBytes[index]!) + 1) & 0x1F
+
+			stateUlidStringBytes[index] = CROCKFORD_BASE32_CHAR_CODES[value]!
+
+			if (value)
+				break if_
+		}
+
+		throw Error(`Overflow when incrementing ULID`)
+	}
+
+	return textDecoder.decode(stateUlidStringBytes) as Ulid
+}
 
 vitest: if (import.meta.vitest) {
 	const { bench } = import.meta.vitest
